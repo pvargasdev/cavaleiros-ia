55a8b91856c84b71ea7a86b781b9b6b80f4d3d7b
P. Vargas <pedrovargas.cba@gmail.com>
2026-03-06 11:40:15 -0400

    Initial commit

---
+++ electron/main.js
@@ -0,0 +1,33 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false, // Simplificando para o trabalho
+    },
+  });
+
+  // Em desenvolvimento, carrega a URL do Vite. 
+  // Na versão final (.exe), carregará o arquivo index.html buildado.
+  const startUrl = process.env.ELECTRON_START_URL || `file://${path.join(__dirname, '../dist/index.html')}`;
+
+  win.loadURL(startUrl);
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
